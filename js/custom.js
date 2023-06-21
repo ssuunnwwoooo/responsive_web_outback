720226b1b5b60c745cb1d89908608a28f49a3402
ssuunnwwoooo <o3olia@naver.com>
2023-06-21 15:51:21 +0900

    outback

---
+++ js/custom.js
@@ -0,0 +1,79 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('.Header').addClass('on')
+        }
+        else {
+            $('.Header').removeClass('on')
+        }
+    });
+
+    const MainSlide = new Swiper('.main_slide', {
+        loop: true,
+        parallax: true,
+        speed: 600,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        on: {
+            slideChangeTransitionStart: function () {
+                $('.MainVisual .dots li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on')
+            }
+        }
+    });
+
+    const ItmSlide = new Swiper('.itm_slide', {
+        loop: true,
+        slidesPerView: 2,
+        spaceBetween: 400,
+        speed: 900,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+    });
+
+    $('.MainItm .arrows .left').on('click', function () {
+        ItmSlide.slidePrev();
+    });
+
+    // document.querySelector('.MainItm .arrows .left').addEventListener('click', () => {
+    //     ItmSlide.slidePrev();
+    // });
+
+    $('.MainItm .arrows .right').on('click', function () {
+        ItmSlide.slideNext();
+        // document.querySelector('.MainItm .arrows .right').addEventListener('click', () => {
+        //     ItmSlide.slideNext();
+        // });
+    });
+
+
+
+
+    $('.MainVisual .arrows .left').on('click', function () {
+        MainSlide.slidePrev();
+    });
+    $('.MainVisual .arrows .right').on('click', function () {
+        MainSlide.slideNext();
+    });
+
+
+    $('.MainVisual .dots li').on('click', function () {
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        MainSlide.slideTo(idx);
+    })
+
+
+
+
+
+})
